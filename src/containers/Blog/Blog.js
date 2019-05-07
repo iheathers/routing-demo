fa9bd9f389eed8801efd82b4ab232a2cd7c5ca99
Pritam Shrestha <shresthapritam7@gmail.com>
2019-05-07 16:57:10 +0545

    Add nested routes / Lazy load routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import asyncComponent from '../../hoc/asyncComponent/asyncComponent';
 
-class Blog extends Component {    
+//import NewPost from './NewPost/NewPost';
 
-    render () { 
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
+class Blog extends Component {
+    render () {
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><a href='/'>Home</a></li>
-                            <li><a href='/new-post'>New Post</a></li>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts/>              
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Switch>
+                    <Route path="/new-post" component={AsyncNewPost} />
+                    <Route path="/posts" component={Posts} />
+                   {/* <Redirect from="/" to="/posts"/> */}
+                </Switch>
             </div>
         );
     }
